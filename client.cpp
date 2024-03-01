a5c905f0452bcf44562144df011614dd9fd00e99
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2024-03-01 21:39:56 +0000

    first commit

---
+++ client.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string serverIP = "localhost";
+    string serverPort = "6606";
+
+    cout << "Hello World" << endl;
+
+    return 0;
+}
